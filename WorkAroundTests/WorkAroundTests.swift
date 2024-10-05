797aba810004e975253903402e5619b0c8d1aa94
Alin Florescu <alin.florescu80@gmail.com>
2024-10-06 01:10:05 +0300

    Initial Commit

---
+++ WorkAroundTests/WorkAroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkAroundTests.swift
+//  WorkAroundTests
+//
+//  Created by Alin Florescu on 06.10.2024.
+//
+
+import Testing
+@testable import WorkAround
+
+struct WorkAroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
