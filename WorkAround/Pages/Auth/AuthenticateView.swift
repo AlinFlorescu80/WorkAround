179625652a801aa0cb730b15464fbabbed978566
Alin Florescu <alin.florescu80@gmail.com>
2025-02-26 01:49:32 +0200

    Added Google Sign In

--- WorkAround/Pages/Auth/AuthenticateView.swift
+++ WorkAround/Pages/Auth/AuthenticateView.swift
@@ -1,10 +1,17 @@
 import SwiftUI
+import FirebaseCore
 import FirebaseAuth
+import GoogleSignIn
+import GoogleSignInSwift
+
+
+
 
 struct AuthenticateView: View {
     @EnvironmentObject var authManager: AuthManager
+    @Environment(\.colorScheme) var colorScheme
     
-    @State private var showInitialView: Bool = true
+    @State private var showInitialView: Bool = true  
     @State private var email: String = ""
     @State private var password: String = ""
     @State private var navigateToHome: Bool = false
@@ -98,17 +105,37 @@ struct AuthenticateView: View {
                     .padding(.top)
                     
                     
-                    
-                    Button("Sign in with Google")
-                    {
-                        
+                    Button {
+                        Task {
+                            if await signInWithGoogle() {
+                                navigateToHome = true
+                                authManager.isSignedIn = true
+                            }
+                        }
+                    } label: {
+                        Text("Sign in with Google")
+                            .foregroundColor(colorScheme == .dark ? .white : .black)
+                            .frame(width: 250)
+                            .padding(.vertical, 8)
+                            .background(alignment: .leading) {
+                                Image("Google")
+                                    .resizable()
+                                    .scaledToFit()
+                            }
                     }
-                    .frame(width: 250, height: 50)
-                    .foregroundColor(.blue)
+                    .buttonStyle(.bordered)
                     
-                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.blue, lineWidth: 2))
-                    .padding()
                     
+//                    Button("Sign in with Google")
+//                    {
+//                        
+//                    }
+//                    .frame(width: 250, height: 50)
+//                    .foregroundColor(.blue)
+//                    
+//                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.blue, lineWidth: 2))
+//                    .padding()
+//                    
                     
                     Button("🍏 Sign in With Apple")
                     {
@@ -160,6 +187,9 @@ struct AuthenticateView: View {
         }
         
     }
+    
+    
+    
   
 }
 
@@ -167,3 +197,52 @@ struct AuthenticateView: View {
 //#Preview {
 //    AuthenticateView()
 //}
+
+
+//MARK: Google Sign-In
+
+enum AuthenticationError: Error {
+    case tokenError(message: String)
+}
+
+@MainActor
+extension AuthenticateView {
+    
+    func signInWithGoogle() async -> Bool {
+        guard let clientID = FirebaseApp.app()?.options.clientID else {
+            fatalError("No Client ID found")
+        }
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let window = windowScene.windows.first,
+              let rootViewController = window.rootViewController else{
+            print("There is no root view controller")
+            return false
+        }
+        
+        
+        do{
+            let userAuthentication = try await GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController)
+            let user = userAuthentication.user
+            guard let idToken = user.idToken else {
+                throw AuthenticationError.tokenError(message: "No ID Token found")
+            }
+            let accessToken = user.accessToken
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken.tokenString, accessToken: accessToken.tokenString)
+            let result = try await Auth.auth().signIn(with: credential)
+            let firebaseUser = result.user
+            print("User \(firebaseUser.uid) signed in with email \(firebaseUser.email ?? "unknown")")
+            return true
+        }
+        catch{
+            print(error.localizedDescription)
+            let errorMessage = error.localizedDescription  // am pus let aici desi nu stiu sigur daca asa trebuia
+            return false
+        }
+        
+        return false
+        
+        
+    }
+}
